391a14df831756f7a8bc47f88c6355a8e64cfed9
aqiph <aqiph818@gmail.com>
2023-09-04 10:10:23 +0800

    Modify similarity search

--- similarity_search.py
+++ similarity_search.py
@@ -20,7 +20,7 @@ from utils.tools import remove_unnamed_columns
 
 
 ### Helper function ###
-def is_similar_by_fingerprint(smiles, smiles_ref, similarity_cutoff):
+def is_similar_by_fingerprint(smiles, smiles_ref, similarity_cutoff=0.7):
     """
     Determine whether smiles is similar to smiles_ref based on ecfp4 fingerprint
     :param smiles: str, SMILES
@@ -32,10 +32,10 @@ def is_similar_by_fingerprint(smiles, smiles_ref, similarity_cutoff):
     fp_2 = get_fingerprint(smiles_ref, fp_method = 'ecfp4')
     dist = cal_fingerprint_distance(fp_1, fp_2, fp_method='ecfp4')
     similarity_score = 1.0 - dist
-    return similarity_score >= similarity_cutoff
+    return similarity_score >= similarity_cutoff, similarity_score
 
 
-def is_similar_by_mcs(smiles, smiles_ref, similarity_cutoff):
+def is_similar_by_mcs(smiles, smiles_ref, similarity_cutoff=0.7):
     """
     Determine whether smiles is similar to smiles_ref based on MCS
     :param smiles: str, SMILES
@@ -44,17 +44,18 @@ def is_similar_by_mcs(smiles, smiles_ref, similarity_cutoff):
     :return: bool, whether smiles is considered to be similar to smiles_ref
     """
     similarity_score = cal_MCS(smiles, smiles_ref)
-    return similarity_score >= similarity_cutoff
+    return similarity_score >= similarity_cutoff, similarity_score
 
 
-def is_similar_by_substructure(smiles, smiles_ref):
+def is_similar_by_substructure(smiles, smiles_ref, substructure_method='SMILES'):
     """
     Determine whether smiles_ref is present in smiles
     :param smiles: str, SMILES
     :param smiles_ref: str, reference SMILES
+    :param substructure_method: str, method used to convert smiles_ref to mol_ref, allowed values include 'SMILES', 'SMARTS'
     :return: bool, whether smiles is considered to be similar to smiles_ref
     """
-    # check reference SMILES
+    # check SMILES
     if not smiles:
         print(f"Error: Invalid SMILES {smiles}")
         return False
@@ -68,20 +69,30 @@ def is_similar_by_substructure(smiles, smiles_ref):
         return False
 
     # determine whether smiles_ref is present in smiles
-    mol_ref = Chem.MolFromSmiles(smiles_ref)
-    return mol.HasSubstructMatch(mol_ref)
+    if substructure_method == 'SMILES':
+        mol_ref = Chem.MolFromSmiles(smiles_ref)
+    elif substructure_method == 'SMARTS':
+        smiles_ref = Chem.MolToSmiles(Chem.MolFromSmiles(smiles_ref))   # Kekulize molecule before get mol as a pattern
+        print(smiles_ref)
+        mol_ref = Chem.MolFromSmarts(smiles_ref)
+    else:
+        raise Exception('Error: Invalid substructure method.')
+
+    hasSubstructure = mol.HasSubstructMatch(mol_ref)
+    return hasSubstructure, int(hasSubstructure)
 
 
-def similarity_search_single_ref(df, smiles_column_name, smiles_ref, method, similarity_cutoff=0.8):
+def similarity_search_single_ref(df, smiles_column_name, smiles_ref, method, **kwargs):
     """
     Determine whether SMILES in df is similar to the reference SMILES.
     Allowed methods include fingerprint, Maximum Common Structure (mcs), and substructure.
     :param df: pd.DataFrame object, library whose smiles_column_name column contains compound SMILES to be searched from.
     :param smiles_column_name: str, name of the SMILES column
     :param smiles_ref: str, reference SMILES
-    :param method: str, method for similarity search, allowed values contain 'fingerprint', 'mcs' and 'substructure'.
+    :param method: str, method for similarity search, allowed values include 'fingerprint', 'mcs' and 'substructure'.
     The method 'substructure' will select compounds that contain the given substructure.
     :param similarity_cutoff: float, similarity cutoff for 'fingerprint' and 'mcs' method.
+    :param substructure_method: str, method used to convert smiles_ref to mol_ref, allowed values include 'SMILES', 'SMARTS'
     :return: df, pd.DataFrame object, library with one additional column specifying whether compounds are similar to the reference compound.
     """
     # check reference SMILES
@@ -100,33 +111,39 @@ def similarity_search_single_ref(df, smiles_column_name, smiles_ref, method, sim
     # Determine whether SMILES in df is similar to the reference SMILES
     method = method.lower()
     if method == 'fingerprint':
+        similarity_cutoff = kwargs.get('similarity_cutoff', 0.7)
         df['is_similar'] = df[smiles_column_name].apply(lambda smiles: is_similar_by_fingerprint(smiles, smiles_ref, similarity_cutoff))
     elif method == 'mcs':
+        similarity_cutoff = kwargs.get('similarity_cutoff', 0.7)
         df['is_similar'] = df[smiles_column_name].apply(lambda smiles: is_similar_by_mcs(smiles, smiles_ref, similarity_cutoff))
     elif method == 'substructure':
-        df['is_similar'] = df[smiles_column_name].apply(lambda smiles: is_similar_by_substructure(smiles, smiles_ref))
+        substructure_method = kwargs.get('substructure_method', 'SMILES')
+        df['is_similar'] = df[smiles_column_name].apply(lambda smiles: is_similar_by_substructure(smiles, smiles_ref, substructure_method))
     else:
         raise Exception('Error: Invalid similarity search method.')
+
+    df[['is_similar', 'Similarity_Score']] = pd.DataFrame(df['is_similar'].values.tolist())
 
     return df
 
 
-def main(input_file, smiles_column_name, smiles_ref, method, similarity_cutoff=0.8, output_file = None, output_option='selected'):
+def main(input_file, smiles_column_name, smiles_ref, method, output_file = None, **kwargs):
     """
     Determine whether SMILES in input file is similar to the reference SMILES.
     Allowed methods include fingerprint, Maximum Common Structure (mcs), and substructure.
     :param input_file: str, path of the input library
     :param smiles_column_name: str, name of the SMILES column
     :param smiles_ref: str, reference SMILES
-    :param method: str, method for similarity search, allowed values contain 'fingerprint', 'mcs' and 'substructure'.
+    :param method: str, method for similarity search, allowed values include 'fingerprint', 'mcs' and 'substructure'.
     The method 'substructure' will select compounds that contain the given substructure.
-    :param similarity_cutoff: float, similarity cutoff for 'fingerprint' and 'mcs' method.
     :param output_file: str, name of the output file
+    :param similarity_cutoff: float, similarity cutoff for 'fingerprint' and 'mcs' method.
+    :param substructure_method: str, method used to convert smiles_ref to mol_ref, allowed values include 'SMILES', 'SMARTS'
     :param output_option: str, options to output data, allowed values include 'selected', 'not_selected' and 'all'
     """
     # read df
     df = pd.read_csv(input_file)
-    COLUMNS = df.columns.tolist()
+    COLUMNS = df.columns.tolist() + ['Similarity_Score']
     assert smiles_column_name in COLUMNS, 'Error: Invalid input SMILES column name.'
 
     # output file
@@ -134,9 +151,10 @@ def main(input_file, smiles_column_name, smiles_ref, method, similarity_cutoff=0
     if output_file is None:
         output_file = basename
     output_file = os.path.join(folder, os.path.splitext(output_file)[0])
+    output_option = kwargs.get('output_option', 'selected')
 
     # similarity search
-    df = similarity_search_single_ref(df, smiles_column_name, smiles_ref, method, similarity_cutoff)
+    df = similarity_search_single_ref(df, smiles_column_name, smiles_ref, method, **kwargs)
     if output_option == 'selected':
         df_sim = pd.DataFrame(df[df['is_similar']], columns = COLUMNS)
     elif output_option == 'not_selected':
@@ -146,11 +164,8 @@ def main(input_file, smiles_column_name, smiles_ref, method, similarity_cutoff=0
     else:
         raise Exception('Error: Invalid output option.')
 
-    # compute mcs score
-    df_sim['MCS_Score'] = df_sim[smiles_column_name].apply(lambda smiles: cal_MCS(smiles, smiles_ref))
-
     # write to file
-    df_sim.sort_values(by=['MCS_Score'], ascending=False, inplace=True)
+    df_sim.sort_values(by=['Similarity_Score'], ascending=False, inplace=True)
     df_sim = df_sim.reset_index(drop=True)
     print('Number of rows:', df_sim.shape[0])
     df_sim = remove_unnamed_columns(df_sim)
@@ -160,10 +175,61 @@ def main(input_file, smiles_column_name, smiles_ref, method, similarity_cutoff=0
 
 if __name__ == '__main__':
 
-    input_file = 'similarity_search/tests/library.csv'
-    smiles_column_name = 'Cleaned_SMILES'
-    smiles_ref = 'NC(=O)SCC(=O)Nc1ccc(S(N)(=O)=O)cc1'
-    method = 'mcs'
-    output_file = 'output.csv'
-    main(input_file, smiles_column_name, smiles_ref, method, similarity_cutoff=0.6, output_file=output_file, output_option='selected')
+    # input_file = 'similarity_search/tests/library.csv'
+    # input_file = 'similarity_search/tests/HTS_forGNN_446663.csv'
+    # smiles_column_name = 'Cleaned_SMILES'
+    # smiles_ref = 'c1ccccc1'
+    # output_file = 'output.csv'
+    ### Method: 'fingerprint' ###
+    # main(input_file, smiles_column_name, smiles_ref, method='fingerprint', output_file=output_file, similarity_cutoff=0.3, output_option='all')
+    ### Method: 'mcs' ###
+    # main(input_file, smiles_column_name, smiles_ref, method='mcs', output_file=output_file, similarity_cutoff=0.3, output_option='all')
+    ### Method: 'substructure' ###
+    # main(input_file, smiles_column_name, smiles_ref, method='substructure', output_file=output_file, substructure_method='SMARTS', output_option='all')
+
+
+    ### Tests ###
+    # smiles = 'C1=CC=CC=C1'
+    # smiles_ref = 'C1=CC=CC=C1'   # True
+
+    # smiles = 'Clc1ccc(CC)cc1'   # True
+    # smiles = 'Clc1ccc(CC)c(C)c1'   # True
+    # smiles = 'c1ccccc1'   # False
+    # smiles_ref = '[*]c1ccc([*])cc1'
+
+    # smiles = 'Clc1c([H])c([H])c(CC)c([H])c1[H]'   # True
+    # smiles = 'Clc1c([H])c([H])c(CC)c(C)c1[H]'   # True
+    # smiles = '[H]c1c([H])c([H])c([H])c([H])c1[H]'   # False
+    # smiles_ref = '[*]c1c([H])c([H])c([*])c([H])c1[H]'
+
+    # smiles = 'NCC'   # True
+    # smiles_ref = '[*]N'
+
+    # smiles = 'C1CC[NH2+]CC1'   # True
+    # smiles = '[NH3+]CC'   # True
+    # smiles_ref = '[*][N+]'
+
+    smiles = 'O=C(C1=CC(N2CCNCC2)=CC=C1C)NC3(CC3)c4c5ccccc5ccc4'
+    # smiles = 'O=C(C1=CC(N2CCNCC2)=CC=C1C)NCCc3c4ccccc4ccc3'
+
+    # smiles_ref = '[*]C1=CC=CC2=C1C=CC=C2'   # False if not kekulized
+    # smiles_ref = '*c1cccc2ccccc12'
+    # smiles_ref = '[*]NC([*])=O'
+    # smiles_ref = '*NC(*)=O'
+    # smiles_ref = 'O=C([*]N)N[*]c1c2ccccc2ccc1'
+
+    # smiles_ref = '[*]C(N[*]c(ccc1)c2c1cccc2)=O'   # True
+    # smiles_ref = 'O=C(c1cccc(N)c1)N[*]c(ccc2)c3c2cccc3'   # True
+    # smiles_ref = 'O=C(c1cccc([*]N)c1)N[*]c(ccc2)c3c2cccc3'
+    smiles_ref = 'c1(cccc2ccccc12)*NC(=O)*'
+
+
+
+    print(is_similar_by_substructure(smiles, smiles_ref, substructure_method='SMARTS'))
+
+
+
+
+
+
 
--- similarity_search/examples/run_similarity_search.py
+++ similarity_search/examples/run_similarity_search.py
@@ -6,7 +6,7 @@ Created on Thu Mar 30 11:14:00 2023
 @author: guohan
 """
 
-import sys
+import sys, time, os
 path_list = sys.path
 module_path = '/Users/guohan/Documents/Codes/Molecule_Analysis'
 if module_path not in sys.path:
@@ -14,12 +14,19 @@ if module_path not in sys.path:
     print('Add module path')
 
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+font = FontProperties()
+font.set_size(10)
 from similarity_search import main
+from utils.tools import remove_unnamed_columns
 
 
-def run_multiple_similarity_search(input_file_library, input_file_ref, id_column_name_ref, smiles_column_name_ref):
+def run_multiple_similarity_search(input_file_library, input_file_ref, id_column_name_ref, smiles_column_name_ref,
+                                   method, similarity_cutoff, dir_analogs):
     """
-
+    Perform similarity search for multiple SMILES from 'input_file_ref'
     """
     # read reference SMILES
     df_ref = pd.read_csv(input_file_ref)
@@ -29,18 +36,92 @@ def run_multiple_similarity_search(input_file_library, input_file_ref, id_column
     print(SMILES)
 
     # run similarity search for each reference cmps
+    total_time = []
     for i, id in enumerate(IDs):
+        start_time = time.time()
         smiles_column_name = 'Cleaned_SMILES'
         smiles_ref = SMILES[i]
         print(f'Start: {id} {smiles_ref}')
-        method = 'substructure'
-        output_file = f'{id}.csv'
-        main(input_file_library, smiles_column_name, smiles_ref, method, similarity_cutoff=0.5, output_file=output_file, output_option='selected')
+        output_file = f'{dir_analogs}/{id}.csv'
+        main(input_file_library, smiles_column_name, smiles_ref, method, output_file, similarity_cutoff=similarity_cutoff, output_option='selected')
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        total_time.append(elapsed_time)
+        print(f'Done, time = {elapsed_time:.2f}')
+    print(f'Mean time is {np.mean(np.array(total_time))}, std is {np.std(np.array(total_time))}')
+
+
+def get_best_score(input_file, dir_analogs, n=0):
+    """
+    Get the top-n most similar compound
+    :param input_file: str, path of the file containing reference compounds
+    :param dir_analogs: str, path of the folder containing analogs
+    :param n: int, indicate the top-n most similar analog
+    """
+    # output file
+    output_file = os.path.splitext(os.path.abspath(input_file))[0]
+
+    df = pd.read_csv(input_file)
+    df_bestScore = pd.DataFrame(df, columns = ['ID', 'SMILES', 'Cleaned_SMILES'])
+
+    df_bestScore['Analog'] = df_bestScore['ID'].apply(lambda cmp: get_analog(cmp, dir_analogs, n))
+    df_bestScore[['Analog_ID', 'Analog_SMILES', 'Analog_Cleaned_SMILES', 'Similarity_Score']] = \
+        pd.DataFrame(df_bestScore['Analog'].tolist())
+    COLUMNS = ['ID', 'SMILES', 'Cleaned_SMILES', 'Analog_ID', 'Analog_SMILES', 'Analog_Cleaned_SMILES', 'Similarity_Score']
+    df_bestScore = pd.DataFrame(df_bestScore, columns = COLUMNS)
+
+    df_bestScore = df_bestScore.reset_index(drop=True)
+    print('Number of rows:', df_bestScore.shape[0])
+    df_bestScore = remove_unnamed_columns(df_bestScore)
+    df_bestScore.to_csv(f'{output_file}_Top{n+1}_{df_bestScore.shape[0]}.csv')
+
+
+def get_analog(cmp, dir_analogs, n):
+    files = os.listdir(dir_analogs)
+    filtered_files = [file for file in files if file.startswith(f'{cmp}_')]
+    df_cmp = pd.read_csv(f'{dir_analogs}/{filtered_files[0]}')
+
+    if df_cmp.shape[0] <= n:
+        return ['', '', '', 0.0]
+
+    return df_cmp.iloc[n, [False, True, True, True, False, False, False, False, False, True]].tolist()
+
+
+def plot_distribution(input_file):
+    """
+
+    """
+    # output file
+    output_file = os.path.splitext(os.path.abspath(input_file))[0]
+
+    df = pd.read_csv(input_file)
+
+    # plot distribution
+    values = df['Similarity_Score'].tolist()
+    output_file = f'{output_file}_similarity_distribution.pdf'
+
+    plt.figure(1)
+    plt.hist(values, 10, range=[0.0, 1.0])
+    plt.xlabel('Similarity Score', fontproperties=font)
+    plt.ylabel('Counts', fontproperties=font)
+    plt.xticks(fontproperties=font)
+    plt.yticks(fontproperties=font)
+
+    plt.savefig(output_file, dpi=300)
+    plt.show()
+    plt.close()
 
 
 if __name__ == '__main__':
-    input_file_library = 'library.csv'
+    # input_file_library = 'library.csv'
+    input_file_library = 'HTS_forGNN_446663.csv'
+
     input_file_ref = 'reference_cmps.csv'
     id_column_name_ref = 'ID'
-    smiles_column_name_ref = 'Cleaned_Scaffold'
-    run_multiple_similarity_search(input_file_library, input_file_ref, id_column_name_ref, smiles_column_name_ref)
+    smiles_column_name_ref = 'Cleaned_SMILES'
+
+    method = 'fingerprint'
+    similarity_cutoff = 0.3
+    dir_analogs = 'HTS_FP0.3'
+    run_multiple_similarity_search(input_file_library, input_file_ref, id_column_name_ref, smiles_column_name_ref,
+                                   method, similarity_cutoff, dir_analogs)
